5b941caca59403b88c62d2533ae8c890ac62d98b
YK Games <yigitkilic093@gmail.com>
2026-05-02 17:37:55 +0300

    Insitaler
    Virus insitaler

---
+++ computer virus/Insıtaler virus.py
@@ -0,0 +1,14 @@
+import time
+import webbrowser
+import requests
+import sys 
+
+print("virus dowland")
+time.sleep (0.3)
+
+webbrowser.open ("")
+
+for i in range(100):
+    sys.stdout.write(".")
+    sys.stdout.flush()
+    time.sleep(0.01)
